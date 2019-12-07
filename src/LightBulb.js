652839aacd48742709a652bf443da3eb41872c89
PJMantoss <joelptoss@gmail.com>
2019-12-07 13:23:19 +0100

    Add LightBulb component

---
+++ src/LightBulb.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+
+export default function LightBulb() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
