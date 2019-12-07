32611667c243188db0c41433447b88ff9dd9f487
PJMantoss <joelptoss@gmail.com>
2019-12-07 13:37:16 +0100

    Update LightBulb

--- src/LightBulb.js
+++ src/LightBulb.js
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
 import "./styles.css";
 
 export default function LightBulb() {
+    let [light, setLight] = useState(0);
+
+    const setOn = () => setLight(0);
+    const setOff = () => setLight(1);
+
+    let fillColor = light === 1 ? "#ffbb73" : "#000000";
+
     return (
         <div>
+            <div>
+                <LightBulbSvg fillColor={fillColor} />
+            </div>
+
+            <button onClick={setOff}>Off</button>
+            <button onClick={setOn}>On</button>
             
         </div>
     )
